6699d70a731591092cdc80b57310dcf020192e97
wong0220 <dlsdnd0220@naver.com>
2022-03-09 17:40:21 +0900

    설문조사 폼 헤더부분 생성 및 기능구현

--- src/features/header/headerSlice.js
+++ src/features/header/headerSlice.js
@@ -1,13 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  value: "",
+  headerContent: {
+    title: "제목 없는 설문지",
+    description: "설문지 설명",
+  },
+};
 
 export const headerSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    setFocus: (state, action) => {
+      state.value = action.payload;
+    },
+
+    setTitle: (state, action) => {
+      state.headerContent.title = action.payload;
+    },
+
+    setDescription: (state, action) => {
+      state.headerContent.description = action.payload;
+    },
+  },
 });
 
-export const {} = headerSlice.actions;
+export const { setFocus, setTitle, setDescription } = headerSlice.actions;
 
 export default headerSlice.reducer;
