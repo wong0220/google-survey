b1cbb05f7e4991ce209fe93cedf109fe496753ff
wong0220 <dlsdnd0220@naver.com>
2022-03-12 23:47:21 +0900

    기타추가시 아이콘 변경 이슈 해결

--- src/component/Etc.js
+++ src/component/Etc.js
@@ -2,14 +2,26 @@ import { Box, TextField } from "@mui/material";
 import { OffEtc } from "../features/content/contentSlice";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import CancelIcon from "@mui/icons-material/Cancel";
-import { useDispatch } from "react-redux";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import { useDispatch, useSelector } from "react-redux";
 
 export function Etc({ contentID }) {
+  const optType = useSelector(
+    (state) => state.content.mltplChcOp[contentID].type
+  );
+
+  function rtrnType(type) {
+    if (type === 30) {
+      return <CircleOutlinedIcon sx={{ color: "gray", mr: 1 }} />;
+    } else if (type === 40) {
+      return <CheckBoxOutlineBlankIcon sx={{ color: " gray", mr: 1 }} />;
+    }
+  }
   const dispatch = useDispatch();
 
   return (
     <Box sx={{ display: "flex", alignItems: "center", mt: 3 }}>
-      <CircleOutlinedIcon sx={{ color: "gray", mr: 1 }} />
+      {rtrnType(optType)}
       <TextField
         defaultValue="기타..."
         variant="standard"
@@ -17,7 +29,7 @@ export function Etc({ contentID }) {
         disabled
       />
       <CancelIcon
-        sx={{ cursor: "pointer" }}
+        sx={{ ml: 1, cursor: "pointer" }}
         onClick={() => {
           dispatch(OffEtc(contentID));
         }}
